15c46d0ea7096c02fceb2aae60b90f78e835ce78
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2024-02-11 12:56:03 +0000

    trabalhando com swagger

--- src/RocketseatAuction.API/Controllers/AuctionController.cs
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
 
 namespace RocketseatAuction.API.Controllers
 {
@@ -7,15 +9,25 @@ namespace RocketseatAuction.API.Controllers
     public class AuctionController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult GetCurrentAuction()
         {
+            var useCase = new GetCurrentAuctionUseCase();
+
+            var result = useCase.Execute();
+
+            if(result is null)
+            {
+                return NoContent();
+            }
 
             // retornos:
                 // Ok() status code 200
                 // Created() criado com sucesso 201
                 // NotFound()
                 // Unauthorized() usuario nao esta autorizado
-            return NotFound("Lussati");
+            return Ok(result);
         }
     }
 }
---
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Writers;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories;
+
+public class RocketseatAuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source=C:\Users\1\OneDrive\Documentos\GitHub\RocketseatAuction\db\leilaoDbNLW.db");
+    }
+}
